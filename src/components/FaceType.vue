36cde319aa49095a7f25c6f111abc8ecb0389c41
zMed-tech <badache.amn@gmail.com>
2021-11-25 12:53:45 +0100

    feat: add two components faceType and mentures

---
+++ src/components/FaceType.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="face-type"></div>
+</template>
+
+<script>
+export default {
+  name: "FaceType",
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/Mentures.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="mentures"></div>
+</template>
+
+<script>
+export default {
+  name: "Mentures",
+};
+</script>
+
+<style lang="scss" scoped></style>
