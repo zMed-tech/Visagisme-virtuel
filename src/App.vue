75a5b3a43f3fc67d67df17a6c2877eb0cb05d924
zMed-tech <badache.amn@gmail.com>
2021-11-25 12:38:46 +0100

    feat: add theNavbar component and import them in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
+  <TheNavbar />
   <router-view />
 </template>
 
+<script>
+import TheNavbar from "@/components/TheNavbar.vue";
+export default {
+  name: "App",
+  components: { TheNavbar },
+};
+</script>
 <style lang="scss">
 * {
   margin: 0;
---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="theNavbar">
+    <div class="logo">
+      <h2>Logo</h2>
+    </div>
+    <ul class="lists">
+      <li class="list">Acceuil</li>
+      <li class="list">Commander Votre Menture</li>
+    </ul>
+    <div class="contact">
+      <button>Contacter-nous</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TheNavbar",
+};
+</script>
+
+<style lang="scss" scoped>
+.theNavbar {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.3rem 0.5rem;
+  border-bottom: 2px solid red;
+  border-radius: 25px;
+
+  & .lists {
+    list-style: none;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+
+    & .list {
+      font-weight: bold;
+      cursor: pointer;
+      margin-right: 2rem;
+      font-size: 1.2rem;
+      transition: all ease-in 0.5s;
+
+      &:hover {
+        color: red;
+        transform: scale(1.2);
+      }
+    }
+  }
+}
+</style>
