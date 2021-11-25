d30841cb90578b3bb5b448aa601ac1f46816f83f
zMed-tech <badache.amn@gmail.com>
2021-11-25 12:54:44 +0100

    feat: import faceType and mentures to Commande view

---
+++ src/views/Commande.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="commande">
+    <FaceType />
+    <Mentures />
+  </div>
+</template>
+
+<script>
+import FaceType from "@/components/FaceType.vue";
+import Mentures from "@/components/Mentures.vue";
+export default {
+  name: "Commande",
+  components: { FaceType, Mentures },
+};
+</script>
+
+<style lang="scss" scoped>
+.commande {
+  height: calc(100vh - $heightNavbar);
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
