c00a5d4c5fa40fe8ddbfa22f24da4829e7d68cfe
zMed-tech <badache.amn@gmail.com>
2021-11-25 14:09:02 +0100

    feat: add article component

---
+++ src/components/Article.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="article">
+    <div class="image">
+      <img
+        :src="require(`@/assets/images/${article.image}`)"
+        alt=""
+        height="100"
+        width="100"
+      />
+    </div>
+    <div class="name">
+      <h3>{{ article.name }}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Article",
+  props: {
+    article: Object,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.article {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  box-shadow: 1px 1px 4px gray;
+}
+</style>
