315755b93dc334f2cccfebd45ecb5253d8f9eb99
zMed-tech <badache.amn@gmail.com>
2021-11-25 14:09:26 +0100

    feat: add faceType component

--- src/components/FaceType.vue
+++ src/components/FaceType.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="face-type"></div>
+  <div class="face-type">
+    <div class="image">
+      <img src="../assets/images/random.jpeg" alt="" height="50" width="50" />
+    </div>
+    <div class="image">
+      <img src="../assets/images/random.jpeg" alt="" height="50" width="50" />
+    </div>
+    <div class="image">
+      <img src="../assets/images/random.jpeg" alt="" height="50" width="50" />
+    </div>
+    <div class="image">
+      <img src="../assets/images/random.jpeg" alt="" height="50" width="50" />
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,4 +21,16 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.face-type {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  & .image {
+    cursor: pointer;
+    margin: 0.5rem 0;
+  }
+}
+</style>
