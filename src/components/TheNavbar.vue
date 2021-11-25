9d11d56a2b78540a80dd4675d5da63ed9365e7e6
zMed-tech <badache.amn@gmail.com>
2021-11-25 14:09:52 +0100

    fix: router link

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -4,10 +4,38 @@
       <h2>Logo</h2>
     </div>
     <ul class="lists">
-      <li class="list" @click="$router.push({ name: 'Home' })">Acceuil</li>
-      <li class="list" @click="$router.push({ name: 'Commande' })">
-        Commander Votre Menture
-      </li>
+      <router-link
+        v-slot="{ navigate, isActive, isExactActive }"
+        :to="{ name: 'Home' }"
+        custom
+      >
+        <li
+          :class="[
+            isActive && 'router-link-active',
+            isExactActive && 'router-link-exact-active',
+            'list',
+          ]"
+          @click="navigate"
+        >
+          Acceuil
+        </li>
+      </router-link>
+      <router-link
+        v-slot="{ navigate, isActive, isExactActive }"
+        :to="{ name: 'Commande' }"
+        custom
+      >
+        <li
+          :class="[
+            isActive && 'router-link-active',
+            isExactActive && 'router-link-exact-active',
+            'list',
+          ]"
+          @click="navigate"
+        >
+          Commander Votre Menture
+        </li>
+      </router-link>
     </ul>
     <div class="contact">
       <button>Contacter-nous</button>
@@ -45,11 +73,17 @@ export default {
       margin-right: 2rem;
       font-size: 1.2rem;
       transition: all ease-in 0.5s;
+      text-decoration: none !important;
+      color: black;
 
       &:hover {
         color: red;
         transform: scale(1.2);
       }
+    }
+
+    & .router-link-exact-active {
+      color: red;
     }
   }
 }
