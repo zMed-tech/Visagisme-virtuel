202e9710452183acf95a70d84fa06cc9dce26111
zMed-tech <badache.amn@gmail.com>
2021-11-25 14:09:41 +0100

    feat: add mentures component

--- src/components/Mentures.vue
+++ src/components/Mentures.vue
@@ -1,11 +1,32 @@
 <template>
-  <div class="mentures"></div>
+  <div class="mentures">
+    <Article
+      :article="{
+        name: 'Menture',
+        image: 'random.jpeg',
+      }"
+    />
+  </div>
 </template>
 
 <script>
+import Article from "@/components/Article";
 export default {
   name: "Mentures",
+  components: {
+    Article,
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.mentures {
+  padding: 0.2rem;
+
+  box-shadow: 0 2px 5px gray;
+  display: grid;
+  grid-template-columns: auto auto auto auto;
+  grid-column-gap: 15px;
+  grid-row-gap: 15px;
+}
+</style>
