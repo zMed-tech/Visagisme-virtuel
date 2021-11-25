a9f812bae3e29aab397904176ec88a71d1f348f6
zMed-tech <badache.amn@gmail.com>
2021-11-25 17:30:46 +0100

    feat: create state

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,48 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    visage: null,
+    genre: null,
+    eyes: null,
+  },
+
+  getters: {
+    getVisage(state) {
+      return state.visage;
+    },
+    getGenre(state) {
+      return state.genre;
+    },
+    getEyes(state) {
+      return state.eyes;
+    },
+  },
+  mutations: {
+    setVisage(state, visage) {
+      state.visage = visage;
+    },
+
+    setGenre(state, genre) {
+      state.genre = genre;
+    },
+
+    setEyes(state, eyes) {
+      state.eyes = eyes;
+    },
+  },
+  actions: {
+    setVisage({ commit }, visage) {
+      commit("setVisage", visage);
+    },
+
+    setGenre({ commit }, genre) {
+      commit("setGenre", genre);
+    },
+
+    setEyes({ commit }, eyes) {
+      commit("setEyes", eyes);
+    },
+  },
   modules: {},
 });
