0c175a7fc64c4165c5dc8671917d5220198b04a2
zMed-tech <badache.amn@gmail.com>
2021-11-25 16:04:59 +0100

    feat: create slideChoice component

---
+++ src/components/SlideChoice.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="slide-choice">
+    <div class="choice" @click="openChoice = !openChoice">
+      <img
+        :src="require(`@/assets/images/${first.image}`)"
+        alt=""
+        width="50"
+        height="50"
+      />
+      <span> {{ first.name }} </span>
+    </div>
+
+    <div v-if="openChoice" class="all-choice">
+      <div
+        v-for="(choice, index) in choices"
+        :key="index"
+        class="choice"
+        @click="first = choice"
+      >
+        <img
+          :src="require(`@/assets/images/${choice.image}`)"
+          alt=""
+          width="50"
+          height="50"
+        />
+        <span> {{ choice.name }} </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SlideChoice",
+  props: {
+    choices: Array,
+  },
+  data() {
+    return {
+      first: this.choices[0],
+      openChoice: true,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.slide-choice {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  margin: 0.5rem 0;
+
+  & .choice {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    cursor: pointer;
+  }
+
+  & .all-choice {
+    box-shadow: 1px 1px 5px gray;
+    padding: 0.1rem 0.2rem;
+    display: flex;
+    visibility: hidden;
+    justify-content: flex-start;
+    align-items: center;
+    transition: all ease-in 1s;
+    width: 0;
+    overflow: hidden;
+
+    & .choice {
+      margin: 0 0.5rem;
+    }
+  }
+  &:hover .all-choice {
+    visibility: visible;
+    width: 100%;
+  }
+}
+</style>
